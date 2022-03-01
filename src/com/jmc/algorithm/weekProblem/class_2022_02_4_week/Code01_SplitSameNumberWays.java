6e462e7fcd381fd9e05b02e8c7485331a476ec27
jiaomengchong <595288608@qq.com>
2022-03-01 21:19:33 +0800

    Create Code01_SplitSameNumberWays.java

---
+++ src/com/jmc/algorithm/weekProblem/class_2022_02_4_week/Code01_SplitSameNumberWays.java
@@ -0,0 +1,22 @@
+package com.jmc.algorithm.weekProblem.class_2022_02_4_week;
+
+/**
+ * 来自微软
+ * 比如，str = "ayxbx"
+ * 有以下4种切法 : a | yxbx、ay | xbx、ayx | bx、ayxb | x
+ * 其中第1、3、4种切法符合：x和y的个数，至少在左右两块中的一块里有相同的数量
+ * 所以返回3
+ * 给定一个字符串str，长度为N
+ * 你有N-1种划分方法，把str切成左右两半，返回有几种切法满足：
+ * x和y的个数，至少在左右两块中的一块里有相同的数量
+ *
+ * @Author jmc
+ * @Description
+ * @Date 2022/3/1 21:11
+ **/
+public class Code01_SplitSameNumberWays {
+    public static int ways1(String str) {
+
+        return 0;
+    }
+}
