c4742863fccc636b84cb31821124a99b45520cdd
jiaomengchong <595288608@qq.com>
2022-03-02 23:36:42 +0800

    Update Code01_SplitSameNumberWays.java
    微软分割字符判断x与y相等问题

--- src/com/jmc/algorithm/weekProblem/class_2022_02_4_week/Code01_SplitSameNumberWays.java
+++ src/com/jmc/algorithm/weekProblem/class_2022_02_4_week/Code01_SplitSameNumberWays.java
@@ -15,8 +15,38 @@ package com.jmc.algorithm.weekProblem.class_2022_02_4_week;
  * @Date 2022/3/1 21:11
  **/
 public class Code01_SplitSameNumberWays {
-    public static int ways1(String str) {
+    public static int ways1(char[] str) {
+        if (str == null || str.length == 0) {
+            return 0;
+        }
 
-        return 0;
+        int xTotal = 0;
+        int yTotal = 0;
+        for (char ch : str) {
+            if (ch == 'x') {
+                xTotal++;
+            }
+            if (ch == 'y') {
+                yTotal++;
+            }
+        }
+
+        int leftX = str[0] == 'x' ? 1 : 0;
+        int leftY = str[0] == 'y' ? 1 : 0;
+        int ans = 0;
+        for (int i = 1; i < str.length; i++) {
+            if (leftX == leftY || (xTotal - leftX) == (yTotal - leftY)) {
+                ans++;
+            }
+            leftX += str[i] == 'x' ? 1 : 0;
+            leftY += str[i] == 'y' ? 1 : 0;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "ayxbxannxnnnnnxhkskskxyxksnxkyxxkxkyyysndghhhdx";
+        char[] str = s.toCharArray();
+        System.out.println(ways1(str));
     }
 }
