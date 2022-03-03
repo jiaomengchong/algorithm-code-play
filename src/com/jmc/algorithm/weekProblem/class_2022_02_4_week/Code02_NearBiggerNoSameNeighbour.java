52e6c958216e9abdf35b6f26a3b761caeb6fe0ac
jiaomengchong <595288608@qq.com>
2022-03-03 23:42:54 +0800

    Create Code02_NearBiggerNoSameNeighbour.java

---
+++ src/com/jmc/algorithm/weekProblem/class_2022_02_4_week/Code02_NearBiggerNoSameNeighbour.java
@@ -0,0 +1,14 @@
+package com.jmc.algorithm.weekProblem.class_2022_02_4_week;
+
+/**
+ * 来自微软
+ * 给定一个正数num，要返回一个大于num的数，并且每一位和相邻位的数字不能相等
+ * 返回达标的数字中，最小的那个
+ * 10^9
+ *
+ * @Author jmc
+ * @Description
+ * @Date 2022/3/3 19:06
+ **/
+public class Code02_NearBiggerNoSameNeighbour {
+}
