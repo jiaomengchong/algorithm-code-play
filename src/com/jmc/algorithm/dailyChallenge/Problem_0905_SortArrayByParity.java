775c2bff43163f563b43dea7a3a0509a726e2250
jiaomengchong <595288608@qq.com>
2022-04-28 12:22:40 +0800

    每日一题：优化常数项，2次遍历

--- src/com/jmc/algorithm/dailyChallenge/Problem_0905_SortArrayByParity.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_0905_SortArrayByParity.java
@@ -41,12 +41,37 @@ public class Problem_0905_SortArrayByParity {
         return ans;
     }
 
+    // 常数项优化:2次遍历得答案
+    public static int[] sortArrayByParity2(int[] nums) {
+        int[] ans = new int[nums.length];
+        if (nums == null || nums.length == 0) {
+            return ans;
+        }
+
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & 1) == 0) {
+                ans[index++] = nums[i];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & 1) != 0) {
+                ans[index++] = nums[i];
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{3, 1, 2, 4};
         int[] ans = sortArrayByParity(nums);
+        int[] ans2 = sortArrayByParity2(nums);
         System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(ans2));
 
         nums = new int[]{0};
         System.out.println(Arrays.toString(sortArrayByParity(nums)));
+        System.out.println(Arrays.toString(sortArrayByParity2(nums)));
     }
 }
