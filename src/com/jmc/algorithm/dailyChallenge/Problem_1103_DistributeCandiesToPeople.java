47583c2a527326f52892369a0b19be4a8e7769bf
jiaomengchong <595288608@qq.com>
2022-05-03 00:16:23 +0800

    lc1103：二分答案解法

--- src/com/jmc/algorithm/dailyChallenge/Problem_1103_DistributeCandiesToPeople.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_1103_DistributeCandiesToPeople.java
@@ -30,13 +30,77 @@ public class Problem_1103_DistributeCandiesToPeople {
         return ans;
     }
 
-    private static int getSum(int num_people) {
-        return num_people * (num_people + 1) / 2;
+    public static int[] distributeCandies1(int candies, int num_people) {
+        if (num_people <= 0) {
+            return new int[]{};
+        }
+
+        int[] res = new int[num_people];
+        int index = 0;
+        while (candies != 0) {
+            // candies = 10 num_people = 4
+            res[index % num_people] += Math.min(index + 1, candies);
+            candies -= Math.min(index + 1, candies);
+            index++;
+        }
+        return res;
+    }
+
+    // 二分答案法
+    public static int[] distributeCandies2(int candies, int num_people) {
+        if (num_people <= 0) {
+            return new int[]{};
+        }
+
+        int[] res = new int[num_people];
+        int num = binarySearch(candies);
+        int rounds = num / num_people;
+        int remain = candies - getSum(1, rounds * num_people, rounds * num_people);
+
+        for (int i = 1; i <= num_people; i++) {
+            int total =  getSum(i, i + (rounds - 1) * num_people, rounds);
+            if (remain > 0) {
+                total += Math.min(remain, i + rounds * num_people);
+                remain -= Math.min(remain, i + rounds * num_people);
+            }
+            res[i - 1] = total;
+        }
+
+        return res;
+    }
+
+    // 等差数列公式
+    public static int getSum(int a1, int an, int n) {
+        return (a1 + an) * n / 2;
+    }
+
+    private static int binarySearch(int candies) {
+        int left = 0;
+        int right = candies;
+        int ans = 0;
+        while (left <= right) {
+            // 2147483647
+            // 1000000000
+            int mid = (left + right) / 2;
+            long total = mid * (mid + 1l) / 2;
+            if (total < candies) {
+                ans = mid;
+                left = mid + 1;
+            } else if (total > candies) {
+                right = mid - 1;
+            } else {
+                ans = mid;
+                break;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int candies = 30;
+        int candies = 1000000000;
         int num_people = 4;
         System.out.println(Arrays.toString(distributeCandies(candies, num_people)));
+        System.out.println(Arrays.toString(distributeCandies1(candies, num_people)));
+        System.out.println(Arrays.toString(distributeCandies2(candies, num_people)));
     }
 }
