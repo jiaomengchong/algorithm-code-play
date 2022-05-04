9b79249ab7de05b60b2fedddd71974406e49795d
jmc <595288608@qq.com>
2022-05-04 13:55:01 +0800

    lc1823 约瑟夫环思想解题

--- src/com/jmc/algorithm/dailyChallenge/Problem_1823_FindTheWinnerOfTheCircularGame.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_1823_FindTheWinnerOfTheCircularGame.java
@@ -24,8 +24,28 @@ public class Problem_1823_FindTheWinnerOfTheCircularGame {
         return queue.peek();
     }
 
+    // 约瑟夫环递归版本
+    public static int findTheWinner1(int n, int k) {
+        if (n == 1) {
+            return n;
+        }
+
+        int ans = (findTheWinner1(n - 1, k) + k) % n;
+        return ans == 0 ? n : ans;
+    }
+
+    public static int findTheWinner2(int n, int k) {
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = (ans + k) % i;
+        }
+        return ans + 1;
+    }
+
     public static void main(String[] args) {
-        int n = 5, k = 2;
+        int n = 235, k = 17;
         System.out.println(findTheWinner(n, k));
+        System.out.println(findTheWinner1(n, k));
+        System.out.println(findTheWinner2(n, k));
     }
 }
