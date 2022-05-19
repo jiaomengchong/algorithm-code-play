11a65f8cb86fcd1e0992d45898b3e319091b73ef
jiaomengchong <595288608@qq.com>
2022-05-19 10:14:31 +0800

    Update Problem_0462_MinimumMovesToEqualArrayElementsII.java

--- src/com/jmc/algorithm/dailyChallenge/Problem_0462_MinimumMovesToEqualArrayElementsII.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_0462_MinimumMovesToEqualArrayElementsII.java
@@ -1,9 +1,12 @@
 package com.jmc.algorithm.dailyChallenge;
 
+import java.util.Arrays;
+
 /**
  * 测试链接：https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/
  */
 public class Problem_0462_MinimumMovesToEqualArrayElementsII {
+    // 暴力递归
     public static int minMoves2(int[] nums) {
         // 输入：nums = [1,10,2,9]
         // 输出：16
@@ -23,6 +26,20 @@ public class Problem_0462_MinimumMovesToEqualArrayElementsII {
 
         return ans;
     }
+
+    // 排序 O(n*logn)
+    public static int minMoves2_(int[] nums) {
+        int ans = 0;
+        Arrays.sort(nums);
+        int mid = nums.length / 2;
+        for (int i = 0; i < nums.length; i++) {
+            ans += Math.abs(nums[i] - nums[mid]);
+        }
+
+        return ans;
+    }
+
+    // TODO 最优解 bfprt O(n)
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
