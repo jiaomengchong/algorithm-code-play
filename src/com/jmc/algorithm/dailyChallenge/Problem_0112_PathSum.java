e1c7489ae47d9633e626147716e186a24875dea0
jiaomengchong <595288608@qq.com>
2022-06-01 23:47:30 +0800

    Create Problem_0112_PathSum.java

---
+++ src/com/jmc/algorithm/dailyChallenge/Problem_0112_PathSum.java
@@ -0,0 +1,61 @@
+package com.jmc.algorithm.dailyChallenge;
+
+/**
+ * 测试链接：https://leetcode.cn/problems/path-sum/
+ */
+public class Problem_0112_PathSum {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        if (hasPathSum(root.left, targetSum - root.val)) {
+            return true;
+        }
+
+        if (hasPathSum(root.right, targetSum - root.val)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        /*root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);*/
+
+        int targetSum = 1;
+        System.out.println(hasPathSum(root, targetSum));
+    }
+}
