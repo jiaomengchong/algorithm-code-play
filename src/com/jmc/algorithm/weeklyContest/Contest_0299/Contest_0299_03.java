0d9d7c33201e3531eede32c08ee86aeb07e4c000
jiaomengchong <595288608@qq.com>
2022-06-30 22:53:11 +0800

    Update Contest_0299_03.java

--- src/com/jmc/algorithm/weeklyContest/Contest_0299/Contest_0299_03.java
+++ src/com/jmc/algorithm/weeklyContest/Contest_0299/Contest_0299_03.java
@@ -5,26 +5,29 @@ package com.jmc.algorithm.weeklyContest.Contest_0299;
  */
 public class Contest_0299_03 {
     public static int maximumsSplicedArray(int[] nums1, int[] nums2) {
-        int[] preSum1 = new int[nums1.length];
-        int[] preSum2 = new int[nums2.length];
-        preSum1[0] = nums1[0];
-        preSum2[0] = nums2[0];
-        for (int i = 1; i < nums1.length; i++) {
-            preSum1[i] = preSum1[i - 1] + nums1[i];
-            preSum2[i] = preSum2[i - 1] + nums2[i];
-        }
-
-        int ans = process(nums1, nums2, 0, preSum1, preSum2);
-        return 0;
+        // s1 = sum1 - (arr1[left1...right1]) + arr2[left2...right2]
+        // s1 = sum1 + (diff[left2-left1]...diff[right2-right1])
+        int ans1 = process(nums1, nums2);
+        int ans2 = process(nums2, nums1);
+        return Math.max(ans1, ans2);
     }
 
-    private static int process(int[] nums1, int[] nums2, int index, int[] preSum1, int[] preSum2) {
-        if (index == nums1.length) {
-            return 0;
+    private static int process(int[] arr1, int[] arr2) {
+        int ans = 0;
+        int N = arr1.length;
+        int preSum = 0;
+        int s1 = 0;
+        for (int i = 0; i < N; i++) {
+            preSum += arr1[i];
+            s1 = Math.max(s1 + arr2[i] - arr1[i], 0);
+            ans = Math.max(ans, s1);
         }
+        return preSum + ans;
+    }
 
-
-
-        return 0;
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{60,60,60};
+        int[] nums2 = new int[]{10,90,10};
+        System.out.println(maximumsSplicedArray(nums1, nums2));
     }
 }
