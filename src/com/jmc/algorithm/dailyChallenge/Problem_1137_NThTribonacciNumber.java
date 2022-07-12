c410e73ab20fe946cbea335eae743426a0bb7377
jiaomengchong <595288608@qq.com>
2022-07-12 21:33:03 +0800

    Update Problem_1137_NThTribonacciNumber.java

--- src/com/jmc/algorithm/dailyChallenge/Problem_1137_NThTribonacciNumber.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_1137_NThTribonacciNumber.java
@@ -1,5 +1,7 @@
 package com.jmc.algorithm.dailyChallenge;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 /**
  * 测试链接：https://leetcode.cn/problems/n-th-tribonacci-number/
  *
@@ -36,8 +38,18 @@ public class Problem_1137_NThTribonacciNumber {
         return dp[n];
     }
 
+    public static int tribonacci2(int n) {
+        int[] map = new int[]{1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415, 223317, 410744, 755476, 1389537, 2555757, 4700770, 8646064, 15902591, 29249425, 53798080, 98950096, 181997601, 334745777, 615693474, 1132436852, 2082876103};
+        return map[n];
+    }
+
     public static void main(String[] args) {
         System.out.println(tribonacci(37));
         System.out.println(tribonacci1(37));
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 38; i++) {
+            sb.append(tribonacci1(i));
+        }
+        System.out.println(sb.toString());
     }
 }
