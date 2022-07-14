4dba71484d77eaff28ac2ffc819519505c215fdd
jiaomengchong <595288608@qq.com>
2022-07-14 14:54:37 +0800

    优化2089为最优解

--- src/com/jmc/algorithm/dailyChallenge/Problem_02089_FindTargetIndicesAfterSortingArray.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_02089_FindTargetIndicesAfterSortingArray.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @Description
  * @Date 2022/6/24 23:09
  **/
-public class Problem_02389_FindTargetIndicesAfterSortingArray {
+public class Problem_02089_FindTargetIndicesAfterSortingArray {
     public static List<Integer> targetIndices(int[] nums, int target) {
         // 输入：nums = [1,2,5,2,3], target = 2
         // 输出：[1,2]
@@ -24,6 +24,28 @@ public class Problem_02389_FindTargetIndicesAfterSortingArray {
             if (nums[i] == target) {
                 ans.add(i);
             }
+        }
+        return ans;
+    }
+
+    public static List<Integer> targetIndices2(int[] nums, int target) {
+        List<Integer> ans = new ArrayList<>();
+        int small = 0, equal = 0;
+        for (int num : nums) {
+            if (num == target) {
+                equal++;
+            } else if (num < target) {
+                small++;
+            }
+        }
+
+        if (equal == 0) {
+            return ans;
+        }
+
+        // 区间[small,small+equal)
+        for (int i = small; i < small + equal; i++) {
+            ans.add(i);
         }
         return ans;
     }
