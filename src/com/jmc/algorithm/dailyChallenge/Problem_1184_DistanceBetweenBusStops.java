2ec841107aad638d0a4802a379e35a17a4da8f8c
jmc <595288608@qq.com>
2022-07-24 22:23:03 +0800

    Update Problem_1184_DistanceBetweenBusStops.java

--- src/com/jmc/algorithm/dailyChallenge/Problem_1184_DistanceBetweenBusStops.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_1184_DistanceBetweenBusStops.java
@@ -1,6 +1,7 @@
 package com.jmc.algorithm.dailyChallenge;
 
 import java.util.ArrayDeque;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -37,10 +38,29 @@ public class Problem_1184_DistanceBetweenBusStops {
         return ans;
     }
 
+        public static int distanceBetweenBusStops2(int[] distance, int start, int destination) {
+            int sum1 = 0, sum2 = 0;
+            if (start > destination) {
+                int tmp = start;
+                start = destination;
+                destination = tmp;
+            }
+
+            for (int i = 0; i < distance.length; i++) {
+                if (i >= start && i < destination) {
+                    sum1 += distance[i];
+                } else {
+                    sum2 += distance[i];
+                }
+            }
+            return Math.min(sum1, sum2);
+        }
+
     public static void main(String[] args) {
         // 0 1 2 3 0 1 2 3
         int[] distance = new int[]{1, 2, 3, 4};
         int start = 1, destination = 3;
         System.out.println(distanceBetweenBusStops(distance, start, destination));
+        System.out.println(distanceBetweenBusStops2(distance, start, destination));
     }
 }
