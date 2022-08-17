d860f92ba9f3114541a69548d49b9d619ad2fd0e
jiaomengchong <595288608@qq.com>
2022-08-17 17:16:52 +0800

    Update Problem_2264_Largest3SameDigitNumberInString.java

--- src/com/jmc/algorithm/dailyChallenge/Problem_2264_Largest3SameDigitNumberInString.java
+++ src/com/jmc/algorithm/dailyChallenge/Problem_2264_Largest3SameDigitNumberInString.java
@@ -6,27 +6,24 @@ package com.jmc.algorithm.dailyChallenge;
 public class Problem_2264_Largest3SameDigitNumberInString {
     public static String largestGoodInteger(String num) {
         int ans = Integer.MIN_VALUE;
-        //输入：num = "6777133339"
-        //输出："777"
+        //输入：num = "89512244403333347431118000333600089144318733366000334093337711191111155870967022201166153829162"
+        //输出："444"
         int N = num.length();
         int right = 0;
         for (int left = 0; left < N; left++) {
             right = Math.max(left, right);
-            while (left + 1 < N && right - left < 3 && num.charAt(left) == num.charAt(left + 1)) {
+            while (left + 1 < N && right < N && right - left < 3 && num.charAt(left) == num.charAt(left + 1)) {
                 right++;
             }
             if (right - left == 3 && num.charAt(left) == num.charAt(left + 1) && num.charAt(left) == num.charAt(left + 2)) {
                 ans = Math.max(ans, Integer.parseInt(num.substring(left, right)));
-            }
-            if (right > left) {
-                left = right - 1;
             }
         }
         return ans == Integer.MIN_VALUE ? "" : ans == 0 ? "000" : String.valueOf(ans);
     }
 
     public static void main(String[] args) {
-        String num = "2300019";
+        String num = "89512244403333347431118000333600089144318733366000334093337711191111155870967022201166153829162";
         System.out.println(largestGoodInteger(num));
     }
 }
