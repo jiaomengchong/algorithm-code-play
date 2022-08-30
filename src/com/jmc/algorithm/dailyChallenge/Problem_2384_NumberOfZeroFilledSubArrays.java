8ad2b77bdb9072e5a27cffb2d0c75e69d755603d
jiaomengchong <595288608@qq.com>
2022-08-30 21:14:35 +0800

    Create Problem_2384_NumberOfZeroFilledSubArrays.java

---
+++ src/com/jmc/algorithm/dailyChallenge/Problem_2384_NumberOfZeroFilledSubArrays.java
@@ -0,0 +1,16 @@
+package com.jmc.algorithm.dailyChallenge;
+
+/**
+ * 测试链接：https://leetcode.cn/problems/number-of-zero-filled-subarrays/
+ */
+public class Problem_2384_NumberOfZeroFilledSubArrays {
+    public static long zeroFilledSubarray(int[] nums) {
+        long ans = 0;
+        int left = 0, right = 0;
+        int N = nums.length;
+        while (left <= right && right < N) {
+
+        }
+        return ans;
+    }
+}
